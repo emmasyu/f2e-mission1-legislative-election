b0703951421f96f5f60f904d379b65ef5ee98568
EmmaSyu <emma831215@gmail.com>
2023-11-20 03:54:44 +0800

    style: 💄 add color and font style

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,19 @@ module.exports = {
   theme: {
     fontFamily:{
       'sans': ['"Noto Sans TC"', ...defaultTheme.fontFamily.sans],
+      'serif': ['"Noto Serif TC"', ...defaultTheme.fontFamily.sans],
       'gaegu': ['Gaegu', ...defaultTheme.fontFamily.sans],
     },
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#00A199',
+        'primary-light': '#14C3BB',
+        'secondary': '#FFDC9E',
+        'secondary-dark': '#14C3BB',
+        'secondary-light': '#FFE9C3',
+        'secondary-tint': '#FCF9E9'
+      },
+    },
   },
   plugins: [],
 }
