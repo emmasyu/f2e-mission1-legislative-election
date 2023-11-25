a31e714e411b838afed7425ed2731e3456a3c715
Emma <emma831215@gmail.com>
2023-11-25 22:15:45 +0800

    feat: 🎸 add navbar menu button toggle

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,38 @@
 <template>
   <div class="fixed left-0 right-0 z-50">
-    <nav class="w-100 px-8 mt-5 mx-5 text-primary font-bold">
+    <nav class="relative w-100 px-8 mt-5 mx-5 text-primary font-bold">
       <div
         class="max-w-[280px] sm:max-w-[1120px] flex justify-between mx-auto py-5 pt-9 sm:max-md:pt-7"
       >
         <h1 class="font-gaegu font-normal text-[40px]">
-          <a href="#" class="p-4 hover:text-primary-light">Miao</a>
+          <a href="#" class="p-4 hover:text-primary-light" @click="closeMenu">Miao</a>
         </h1>
-        <button class="pt-2 md:hidden">
-          <span class="material-symbols-outlined text-[40px]">menu</span>
+        <button class="pt-2 sm:hidden" @click="toggleMenu">
+          <span class="material-symbols-outlined text-[40px]">{{ currentMenuIcon }}</span>
         </button>
-        <ul class="flex items-center gap-1 lg:gap-6 max-md:hidden">
-          <li v-for="item in navItem" :key="item.tag">
+        <ul
+          class="flex items-center gap-4 sm:gap-1 lg:gap-6 max-sm:flex-col max-sm:w-full max-sm:left-0 max-sm:absolute top-32"
+          :class="{ 'max-sm:hidden': currentMenuIcon === 'menu' }"
+        >
+          <li
+            v-for="item in navItem"
+            :key="item.tag"
+            class="max-sm:bg-white max-sm:hover:bg-secondary cursor-pointer max-sm:w-full text-center rounded-full max-sm:shadow-[3px_3px_0_#F4CA80]"
+            @click="closeMenu"
+          >
             <a
               :href="item.tag"
-              class="px-2 py-5 hover:text-primary-light hover:decoration-primary-light hover:underline hover:underline-offset-4"
+              class="px-2 py-5 hover:text-primary-light hover:decoration-primary-light hover:underline hover:underline-offset-4 max-sm:block"
               >{{ item.name }}</a
             >
           </li>
-          <li class="flex">
+          <li class="flex sm:max-md:hidden max-sm:gap-4 max-sm:mt-4 max-sm:pb-4">
             <a
               href="#"
-              class="hover:opacity-50"
+              class="hover:opacity-70 max-sm:drop-shadow-[3px_3px_0_#F4CA80]"
               v-for="(icon, i) in [IconFacebook, IconInstagram, IconYoutube]"
               :key="i"
+              @click="closeMenu"
               ><component :is="icon" color="#00A199" bgColor="white"
             /></a>
           </li>
@@ -34,6 +43,7 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import IconFacebook from './icons/IconFacebook.vue'
 import IconInstagram from './icons/IconInstagram.vue'
 import IconYoutube from './icons/IconYoutube.vue'
@@ -45,6 +55,14 @@ const navItem: { tag: string; name: string }[] = [
   { tag: '#donation', name: '小額捐款' },
   { tag: '#message', name: '服務信箱' }
 ]
+
+const currentMenuIcon = ref('menu')
+function toggleMenu() {
+  currentMenuIcon.value = currentMenuIcon.value === 'menu' ? 'close' : 'menu'
+}
+function closeMenu() {
+  currentMenuIcon.value = 'menu'
+}
 </script>
 
 <style scoped>
