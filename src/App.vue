ad893c45a00d67ce85e012a14a88ea619ca50520
Emma <emma831215@gmail.com>
2023-11-25 20:47:38 +0800

    feat: 🎸 add go top button

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <NavBar />
   <RouterView />
   <FooterLayout />
+  <TopButton />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
 import FooterLayout from './components/FooterLayout.vue'
+import TopButton from './components/TopButton.vue'
 </script>
---
+++ src/components/TopButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="fixed bottom-8 right-8 w-12 h-12 bg-primary-light z-50 rounded-full shadow-[0_0_16px_#D1D1D1] cursor-pointer"
+    @click="scrollToTop"
+    v-if="scrollY > 100"
+  >
+    <span class="material-symbols-outlined text-5xl text-white"> expand_less </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+function scrollToTop() {
+  window.scrollTo(0, 0)
+}
+
+const scrollY = ref(0)
+
+onMounted(() => {
+  window.addEventListener('scroll', () => {
+    scrollY.value = window.scrollY
+  })
+})
+</script>
