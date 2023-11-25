91a57a1d0d5c42b7632cdaeea8055047d83db7ed
Emma <emma831215@gmail.com>
2023-11-26 05:46:50 +0800

    refactor: 💡 change div to button element

--- src/components/TopButton.vue
+++ src/components/TopButton.vue
@@ -1,11 +1,11 @@
 <template>
-  <div
+  <button
     class="fixed bottom-8 right-8 w-12 h-12 bg-primary-light z-50 rounded-full shadow-[0_0_16px_#D1D1D1] cursor-pointer"
     @click="scrollToTop"
     v-if="scrollY > 100"
   >
     <span class="material-symbols-outlined text-5xl text-white"> expand_less </span>
-  </div>
+  </button>
 </template>
 
 <script setup lang="ts">
